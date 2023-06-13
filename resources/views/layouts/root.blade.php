34598655d60ecd1f32e3dfda726c753a2765cd10
leandroccama <abelurussan@gmail.com>
2023-06-13 14:55:31 -0500

    add

--- resources/views/layouts/root.blade.php
+++ resources/views/layouts/root.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>SbcMedic</title>
-    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
+    <title>SBCMedic</title>
+    <link rel="shortcut icon" href="{{ asset('landing_assets/images/favicon.ico') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite(['resources/js/root.js'])
 {{--    <link href="{{ asset('assets/libs/@iconscout/unicons/css/line.css') }}" type="text/css" rel="stylesheet" />--}}
